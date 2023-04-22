1a5b2decc1c9fb3e726c5fc408c417dc61daf6bf
Jojo Sutton <jojo.sutton@adcellerant.com>
2023-04-21 19:15:03 -0500

    remove target

--- 2022/day-4/target/debug/day-4.d
+++
@@ -1,1 +0,0 @@
-/Users/jojosutton/Repos/aoc/target/debug/day-4: /Users/jojosutton/Repos/aoc/src/main.rs
--- 2022/day-4/target/debug/deps/day_4-0d69c2c1426fa1f3.d
+++
@@ -1,8 +0,0 @@
-/Users/jojosutton/Repos/aoc/target/debug/deps/day_4-0d69c2c1426fa1f3.rmeta: src/main.rs Cargo.toml
-
-/Users/jojosutton/Repos/aoc/target/debug/deps/day_4-0d69c2c1426fa1f3.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
