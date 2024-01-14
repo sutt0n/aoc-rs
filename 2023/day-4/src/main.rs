74d63bd123db0714d898483e0476e913cf98e4d0
Jojo Sutton <jojo.sutton@adcellerant.com>
2024-01-14 11:54:59 -0600

    feat: day 4, part 2

--- 2023/day-4/src/main.rs
+++ 2023/day-4/src/main.rs
@@ -1,9 +1,12 @@
+use std::cmp::min;
+use std::collections::{HashSet, HashMap};
 use std::fs::File;
+use std::hash::Hash;
 use std::io::prelude::*;
 
-fn concat(vec: &[u32]) -> u32 {
-    vec.iter().fold(0, |acc, &x| acc * 10 + x)
-}
+// fn concat(vec: &[u32]) -> u32 {
+//     vec.iter().fold(0, |acc, &x| acc * 10 + x)
+// }
 
 fn read_file(path: &str) -> String {
     let mut file = File::open(path).unwrap();
@@ -29,12 +32,12 @@ fn parse_card_points(winning_numbers: Vec<u32>, play_numbers: Vec<u32>) -> u32 {
     winning_numbers_played.len() as u32
 }
 
-fn part1(input: &str) {
-    let card_points: Vec<u32> = input
+fn get_card_points(input: &str) -> Vec<u32> {
+    input
         .lines()
         .into_iter()
         .map(|line| {
-            let (card_id, numbers) = line.split_once(":").unwrap();
+            let (_, numbers) = line.split_once(":").unwrap();
             let (play_numbers, winning_numbers) = numbers.trim().split_once('|').unwrap();
 
             let play_numbers: Vec<u32> = play_numbers
@@ -63,8 +66,11 @@ fn part1(input: &str) {
 
             parse_card_points(winning_numbers, play_numbers)
         })
-        .collect();
+        .collect()
+}
 
+fn part1(input: &str) {
+    let card_points = get_card_points(input);
     let points_sum = card_points
         .iter()
         .map(|points| {
@@ -79,13 +85,47 @@ fn part1(input: &str) {
 }
 
 fn part2(input: &str) {
+    let card_points = get_card_points(input);
+
+    let winnings_numbers: Vec<Vec<u32>> = card_points
+        .iter()
+        .enumerate()
+        .map(|(idx, points)| {
+            let mut v = Vec::new();
+            for x in 1..=*points {
+                v.push(idx as u32 + x + 1);
+            }
+            v
+        })
+        .collect();
+
+    let mut card_values: HashMap<u32, u32> = HashMap::new();
+
+    for (idx, _) in card_points.iter().enumerate() {
+        let card_id = idx as u32 + 1;
+        *card_values.entry(card_id).or_default() += 1;
+    }
+
+    for (idx, winnings) in winnings_numbers.iter().enumerate() {
+        let card_id = idx as u32 + 1;
+        for card in winnings {
+            *card_values.entry(*card).or_default() += *card_values.get(&(&card_id)).unwrap();
+        }
+    }
+
+    let sum: u32 = card_values
+        .values()
+        .into_iter()
+        .sum();
+
+    println!("sum {:?}", sum);
 }
 
 fn main() -> Result<(), std::io::Error> {
     let input = read_file("input.txt");
 
     part1(&input);
-    // part2(&input);
+    part2(&input);
 
     Ok(())
 }
